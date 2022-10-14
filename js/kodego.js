ac9c7766ab71a758ee0ab5de38782a577f132bf1
andrewfalsado <108734022+andrewfalsado@users.noreply.github.com>
2022-10-14 20:38:32 +0800

    added swtich case sample

--- js/kodego.js
+++ js/kodego.js
@@ -1,5 +1,56 @@
 function getAreaOfCircle(radius){
     const pi = 3.14;
-    var area = (pi * radius) * (pi * radius);
+    var area = (pi * radius) ** 2;
     return area;
 }
+
+function doCheckOddOrEven(value){
+    if(value % 2 == 0){
+        return "ODD";
+    }else{
+        return "EVEN";
+    }
+}
+
+function doCheckVowelOrConsonant(char){
+    char = char.toLowerCase();
+    if(char == "a" || char == "e" || char == "i" || char == "o" || char == "u"){
+        return "vowel";
+    }else{
+        return "consonant";
+    }
+}
+
+function getFlowerColor(flower){
+    switch(flower){
+        case "rose":
+            return "red";
+        case "violet":
+            return "blue";
+        case "sunflower":
+            return "yellow";
+        default: 
+            return "unknown";
+    }
+}
+
+function getCalendarMonthDays(monthName){
+    monthName = monthName.toLowerCase();
+    switch(monthName){
+        case "september":
+        case "april":
+        case "june": 
+        case "november": 
+            return 30;
+        case "february":
+            return 28;
+        default:
+            return 31;
+    }
+}
+
+
+
+
+
+
