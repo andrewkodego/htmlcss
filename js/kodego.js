eea98e03f5f86207c68ef993de6598c41a3ecb53
andrewfalsado <108734022+andrewfalsado@users.noreply.github.com>
2022-10-17 21:20:54 +0800

    additional array methods sample

--- js/kodego.js
+++ js/kodego.js
@@ -177,18 +177,15 @@ function doFibonacciSequence(limit){
 
     let numbers = [0,1];
     let newValue = 0;
-    let n1 = 0;
-    let n2 = 1;
 
     if(limit <= 1){
         number = numbers.pop();
     }
 
     for(let i=0; i < limit-2; i++){
-        newValue = n1 + n2;
-        n1 = n2;
-        n2 = newValue;
+        newValue = numbers[i] + numbers[i+1];
         numbers.push(newValue);
+        console.log(numbers);
     }
 
     return numbers;
